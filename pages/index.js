5bc6584f7f90cff9c13f49b39ecf6eb18fe297db
Felipe Merchan <felipe-sc@hotmail.es>
2021-04-18 20:43:23 -0500

    Se agregó el archivo de estilos de la librería aos al proyecto

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import Aos from 'aos'
-import "aos/dist/aos.css"
 
 import { Hero } from '../src/components/Hero'
 import styles from '../src/assets/styles/pages/index.module.scss'
